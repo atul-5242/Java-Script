295320be287dd276c1c8510805572af01898e565
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2023-02-21 21:47:27 +0530

    Complete till video no.7

---
+++ Conditional-expressions-in-JavaScript.js
@@ -0,0 +1,37 @@
+// prompt and alert:---------->
+
+// prompt always ask 
+// But alert alwatys tell
+
+//ex:- let a=prompt("What is your age?");
+// let a='6';
+// console.log(typeof a);
+// a=Number.parseInt(a);
+// console.log(typeof a);
+
+
+// ---------------------------Conditional Statement-----------------------------------------------
+
+let a=6;
+if (a>0) {
+    console.log("YES")
+}
+else{
+    console.log("No")
+}
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Here Switch case is same as C language.
+// And if else all are same as C language.
+
+
+// Ternary operator are also same as C language.
+a>0?console.log(a):console.log("No")
---
+++ JavaScript-Operators-and-Expressions.js
@@ -0,0 +1,6 @@
+// I let the practice 5 video.
+// https://drive.google.com/drive/folders/1YBKLxcgFe7lEqZ7RMIZT4lH28i22vLkN
+// This above link is enough
+// Full Notes of JS
+// https://drive.google.com/drive/folders/1RcyvvOAxJtNc_mPLt9g-kTHIMUZ3uD8z
+
