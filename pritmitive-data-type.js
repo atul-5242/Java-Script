2cb1b7b3e008785e5325694cbedfe3ca91d694ca
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2023-02-21 19:57:55 +0530

    Complete till video no.4

---
+++ pritmitive-data-type.js
@@ -0,0 +1,57 @@
+// nn bb ss u        --primitive data type
+
+let a=null;
+let b=342;
+let c=true//can also be false
+let d=BigInt("786");
+let e="Harry";
+let f=Symbol("I am Symbol");
+let g=undefined;
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+console.log(a,b,c,d,e,f,g);
+// console.log(typeof a);
+
+// Non Primitive Data Type:-----------(Objects in Js is called Non-primitive data type.)-------------------------------->
+
+// Objects are kye value pairs.
+const iteam={
+    "Harry":true,
+    "Shubh":false,
+    "number":67,
+    "Ram":undefined,
+}
+
+console.log(iteam["Harry"],iteam["Ram"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
