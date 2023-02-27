6338d7d36c93a50b07595b0743a5546d177a38e1
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2023-02-28 02:33:00 +0530

    complete till video 18 in JS.

--- Arrays.js
+++ Arrays.js
@@ -51,3 +51,96 @@ console.log(a,"W:",w)
 
 let t = a.unshift(78)//add element in the beginning of the array.
 console.log("a:",t, a)
+
+
+
+
+// Some More JavaScript Array Methods | JavaScript Tutorial in Hindi #18:---------------------->
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+// let num_even_more = [211, 212, 213, 214, 415, 416, 417, 418, 419]
+// console.log(num.length)
+// delete num[0]
+// console.log(num.length)
+
+// let newArray = num.concat(num_more, num_even_more)
+// console.log(newArray)
+// console.log(num, num_more)
+
+// sort method
+// let compare = (a, b)=>{
+//   return b - a
+// }
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// num.sort(compare)
+// num.reverse()
+// console.log(num)
+
+// Splice and Slice
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// let deletedValues = num.splice(2, 4, 1021, 1022, 1023, 1024, 1025)
+// console.log(num)
+// console.log(deletedValues)
+
+// let newNum = num.slice(3)
+// let newNum = num.slice(3, 5)
+// console.log(newNum)
+
+
+
+
+
+
+
+
+
+// My:code:------------------------------->
+
+console.log("From now onwards we are going to learn more methods.")
+
+let num=[1,2,3,4,5,6,7,8,9,10]
+console.log(num.length)//l2
+delete num[0]
+console.log(num.length)//l1
+// In both l2==l1 it is in this language.
+let a1=[1,3,4,1,5,76]
+let a2=[45,3,3,34,2,2,4,2,2]
+let a3=[45,3,3,34,2,2,4,2,2]
+let newArray_here1=a1.concat(a2,a3);//it can take more than one argument.
+let newArray_here=a1.concat(a2);
+console.log(newArray_here)
+
+
+// sort method sort in alphabetically:-------------------------------->
+
+l1=[3,1,141,3,2,,5,765,7,435,34,8]
+l1.sort()
+console.log(l1)//sort in alphabetically order.
+
+
+// if I want to sort in assending or decending order than we have to sort by using to give a function to sort function as an argument.
+
+let compare=(a,b)=>{
+    // return a-b//assending
+    return b-a//decending
+}
+console.log(l1.sort(compare))
+
+
+l1.reverse()
+
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// num.sort(compare)
+// num.reverse()
+// console.log(num)
+
+// Splice and Slice
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// let deletedValues = num.splice(2, 4, 1021, 1022, 1023, 1024, 1025)
+// console.log(num)
+// console.log(deletedValues)
+
+// let newNum = num.slice(3)
+// let newNum = num.slice(3, 5)
+// console.log(newNum)
